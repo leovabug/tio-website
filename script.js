c6ea46e2a64b67634679859bb95af81e8b2fa11c
leova <leovadork@gmail.com>
2026-01-13 20:51:03 -0600

    more modern styling sample

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//test
+const menuToggle = document.querySelector('.menu-toggle');
+const navUl = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
